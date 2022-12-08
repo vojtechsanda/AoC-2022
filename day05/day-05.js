84d1f155ba7c65a669771b2c7f8855e03e4e5383
Vojtěch Šanda <vojtech.sanda@gmail.com>
2022-12-08 22:36:42 +0100

    D05: Finished

--- day05/day-05.js
+++ day05/day-05.js
@@ -5,8 +5,8 @@ try {
   const linesDev = dataDev.split('\r\n');
 
   console.log('----  TESTING DATA  ----');
-  testIt('Part 1', part1(linesDev), 2);
-  testIt('Part 2', part2(linesDev), 4);
+  testIt('Part 1', part1([...linesDev]), 'CMZ');
+  testIt('Part 2', part2([...linesDev]), 'MCD');
 
   console.log();
 
@@ -14,15 +14,90 @@ try {
   const linesProd = dataProd.split('\r\n');
 
   console.log('---- PRODUCTION DATA ----');
-  testIt('Part 1', part1(linesProd), 496);
-  testIt('Part 2', part2(linesProd), -Infinity);
+  testIt('Part 1', part1([...linesProd]), 'FWSHSPJWM');
+  testIt('Part 2', part2([...linesProd]), -Infinity);
 } catch (err) {
   console.error(err);
 }
 
-function part1(lines) {}
+function part1(lines) {
+  const dividerIndex = lines.findIndex((line) => line === '');
+  const config = lines.splice(0, dividerIndex + 1).slice(0, -2);
+  const stacks = [];
 
-function part2(lines) {}
+  const regex = /(...) ?/gm;
+  config.forEach((line) => {
+    const containers = [];
+    let container;
+    while (([_, container] = regex.exec(line) ?? []).length) {
+      containers.push(container);
+    }
+
+    containers.forEach((container, i) => {
+      if (!stacks[i]) {
+        stacks[i] = [];
+      }
+      if (container[1].trim()) {
+        stacks[i].unshift(container[1]);
+      }
+    });
+  });
+
+  lines.forEach((line) => {
+    const [quantity, from, to] = line
+      .split(' ')
+      .filter((_, index) => index % 2)
+      .map((el) => Number(el));
+
+    for (let i = 0; i < quantity; i++) {
+      const item = stacks[from - 1].splice(-1, 1)[0];
+      stacks[to - 1].push(item);
+    }
+  });
+
+  return stacks.map((stack) => stack.splice(-1, 1)[0]).join('');
+}
+
+function part2(lines) {
+  const dividerIndex = lines.findIndex((line) => line === '');
+  const config = lines.splice(0, dividerIndex + 1).slice(0, -2);
+  const stacks = [];
+
+  const regex = /(...) ?/gm;
+  config.forEach((line) => {
+    const containers = [];
+    let container;
+    while (([_, container] = regex.exec(line) ?? []).length) {
+      containers.push(container);
+    }
+
+    containers.forEach((container, i) => {
+      if (!stacks[i]) {
+        stacks[i] = [];
+      }
+      if (container[1].trim()) {
+        stacks[i].unshift(container[1]);
+      }
+    });
+  });
+
+  lines.forEach((line) => {
+    const [quantity, from, to] = line
+      .split(' ')
+      .filter((_, index) => index % 2)
+      .map((el) => Number(el));
+
+    const items = stacks[from - 1].splice(-1 * quantity);
+
+    items.forEach((item) => {
+      stacks[to - 1].push(item);
+    });
+
+    // console.log(line, [stacks[from - 1], stacks[to - 1]], quantity, from, to, items);
+  });
+
+  return stacks.map((stack) => stack.splice(-1, 1)[0]).join('');
+}
 
 function testIt(testName, actual, expected) {
   const passed = expected === actual;
